aa8f12e001e1f980607af0698c066cad50445f3f
Babek Asadli <babekasadli@protonmail.com>
2024-05-05 22:21:39 +0400

    todo is done

--- src/main/java/AISTGroup/ders1/task6/Calculator.java
+++ src/main/java/AISTGroup/ders1/task6/Calculator.java
@@ -5,43 +5,78 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("=====CALCULATOR=====\nDiqqət: 'q' yazaraq proqramdan çıxış edə bilərsiniz");
+        double result = 0;
+        while (true){
+            System.out.print("Birinci ədədi daxil edin: ");
+            String firstEnterance = sc.next();
+
+            if (firstEnterance.equals("q")) {
+                System.out.println("Proqram sonlandırıldı");
+                System.exit(0);
+            }
+            else if (!(firstEnterance.matches("[0-9]+"))) {
+                System.out.println("Yalnız rəqəm daxil edilməlidir!");
+            }
+            else{
+                result = Double.parseDouble(firstEnterance);
+                break;
+            }
+        }
 
         while (true) {
-            System.out.print("Birinci ədədi daxil edin: ");
-            double num1 = sc.nextDouble();
-
-            System.out.print("Ikinci ədədi daxil edin: ");
-            double num2 = sc.nextDouble();
-
-            System.out.print("Əməliyyatı daxil edin (+, -, *, /): ");
+            System.out.print("  (+, -, *, /): ");
             char operator = sc.next().charAt(0);
 
-            double result;
+            if (operator == 'q') {
+                System.out.println("Proqram sonlandırıldı");
+                break;
+            }
+
+            if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                System.out.println("Düzgün əməliyyat daxil edilməyib!");
+                continue;
+            }
+
+            double nextNum = 0;
+
+            while (true){
+                System.out.print("Növbəti ədədi daxil edin: ");
+                String nextEnterance = sc.next();
+
+                if (nextEnterance.equals("q")) {
+                    System.out.println("Proqram sonlandırıldı");
+                    System.exit(0);
+                }
+                else if (!(nextEnterance.matches("[0-9]+"))){
+                    System.out.println("Yalnız rəqəm daxil edilməlidir!");
+                }else{
+                    nextNum = Double.parseDouble(nextEnterance);
+                    break;
+                }
+            }
 
             switch (operator) {
                 case '+':
-                    result = num1 + num2;
+                    result += nextNum;
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result -= nextNum;
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result *= nextNum;
                     break;
                 case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
+                    if (nextNum != 0) {
+                        result /= nextNum;
                     } else {
                         System.out.println("Sıfıra bölə bilməz!");
                         continue;
                     }
                     break;
-                default:
-                    System.out.println("Düzgün əməliyyat daxil edilməyib!");
-                    continue;
             }
 
-            System.out.println("Nəticə: " + result);
+            System.out.print("Nəticə: " + result);
         }
     }
 }
